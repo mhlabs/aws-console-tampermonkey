035dc8b1a41db589175d7b604d89bd2473e377b2
Lars Jacobsson <lars.jacobsson@mathem.se>
2020-03-23 20:22:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       mhlabs
 // @match        https://*.console.aws.amazon.com/*
 // @grant        none
 // ==/UserScript==
